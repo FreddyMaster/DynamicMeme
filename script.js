61d672e06ff5484a7c4684cb23594be33e9b139e
FreddyMaster <42700111+FreddyMaster@users.noreply.github.com>
2024-04-04 19:50:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function changeText() {
+    const textElement = document.getElementById("myText");
+    const yesAJElement = document.getElementById("yesAJ");
+    setInterval(() => {
+       const currentTime = new Date();
+       const currentHour = currentTime.getHours();
+       const currentMinute = currentTime.getMinutes();
+       const amPm = currentHour >= 12 ? "PM" : "AM";
+       const hour = currentHour % 12 || 12;
+       textElement.textContent = `Tim it's ${hour}:${currentMinute.toString().padStart(2, '0')} ${amPm}. Time to play Overwatch 2`;
+       yesAJElement.textContent = "Yes AJ";
+    }, 1000);
+   }
+
+
+ 
+   function shareImage() {
+ html2canvas(document.querySelector('.meme')).then(function(canvas) {
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'meme.png';
+    link.click();
+
+    // Display instructions on how to share the image on Discord
+    alert("Your meme has been downloaded! To share it on Discord, upload the image to any image hosting service and then paste the link here.");
+ });
+}
