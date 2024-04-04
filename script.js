af6b2d490c440af1669a7c6432f376bd730b240a
FreddyMaster <42700111+FreddyMaster@users.noreply.github.com>
2024-04-04 19:58:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function changeText() {
        const currentMinute = currentTime.getMinutes();
        const amPm = currentHour >= 12 ? "PM" : "AM";
        const hour = currentHour % 12 || 12;
-       textElement.textContent = `Tim it's ${hour}:${currentMinute.toString().padStart(2, '0')} ${amPm}. Time to play Overwatch 2`;
+       textElement.textContent = `Tim it's ${hour}:${currentMinute.toString().padStart(2, '0')} ${amPm}. Time to play Dark Souls 2`;
        yesAJElement.textContent = "Yes AJ";
     }, 1000);
    }
